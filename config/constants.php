ac7bdf775fc5e2e800eab3feae6012673e0c364e
Hxtreme <rwpttech@gmail.com>
2025-12-10 17:11:59 +0530

    config

---
+++ config/constants.php
@@ -0,0 +1,18 @@
+<?php
+	return [
+		'project_url' => 'https://dbta.rtpltech.in/',
+		'project_name' => 'Dbta',
+		'company_name' => 'Dbta',
+		'invoice_name' => '',
+		'project_email' => 'customercare@dbta.com',
+		'project_mobile' => '+91-123-4567898',
+		'gst' => '',
+		'iec' => '',
+		'pan' => '',
+		'cin' => '',
+		'tin' => '',
+		'pincode'=> ''
+		
+		];
+	
+?>
